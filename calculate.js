be8ac7c8a26a47627f23aba9c8224efe1d09b884
Marco Antonio <67771574+tonho991@users.noreply.github.com>
2024-07-11 11:20:43 -0400

    11/07

--- calculate.js
+++ calculate.js
@@ -1,23 +1,47 @@
- $("#modal").modal("show")
+$("#modal-telha").modal("show");
 
 $("#btn-calc").on("click", () => {
   calculate();
 });
 
-$("#tint-btn-calc").on("click", ()=> {
+$("#tint-btn-calc").on("click", () => {
   calcularTinta();
 });
 
-$("#unknow-area-button").change(function() {
-  if($(this).is(":checked")){
+$("#tile-btn-calc").on("click", () => {
+  calcularTelha()
+})
+
+$("#unknow-area-button").change(function () {
+  if ($(this).is(":checked")) {
     $("#tint-convenient-width-box").css("display", "block");
     $("#tint-convenient-height-box").css("display", "block");
-    $("#tint-convenient-area").css("display", "none")
+    $("#tint-convenient-area").css("display", "none");
   } else {
     $("#tint-convenient-width-box").css("display", "none");
     $("#tint-convenient-height-box").css("display", "none");
-    $("#tint-convenient-area").css("display", "block")
+    $("#tint-convenient-area").css("display", "block");
   }
+});
+
+$("#roof-type").change(function () {
+  let type = $(this).val();
+  $("#ceramica-options").hide();
+  $("#measurements").hide();
+  $("#tiles-type").empty();
+
+  $("#tiles-type").append(`<option value="" disabled selected>Selecione o Tipo de Telha ${tileData[type].title} </option>`)
+  tileData[type].list.forEach((tile) => {
+    $("#tiles-type").append(
+      `<option value="${tile.unit}">${tile.name}</option>`
+    );
+  });
+
+  $('#tiles-options').show();
+});
+
+$("#tiles-type").change(function () {
+  $("#calculate-content").show();
 });
 
 function calculate() {
@@ -28,22 +52,28 @@ function calculate() {
   //exemplo de piso 120x60
 
   let itemArea = (120 / 100) * (100 / 60);
-  let convenientArea = (convenientWidth) * (convenientLength);
+  let convenientArea = convenientWidth * convenientLength;
 
   let items = (convenientArea / itemArea).toFixed(0);
 
-  let finalItems = lossPercent = 0 ? items : (items * (1 + (lossPercent / 100))).toFixed(0);
+  let finalItems = (lossPercent = 0
+    ? items
+    : (items * (1 + lossPercent / 100)).toFixed(0));
 
-  $("#result").html(`Quantidade de peças necessárias ${lossPercent <= 0 ? "" : ", considerando a taxa de perca"}: ${finalItems}`)
+  $("#result").html(
+    `Quantidade de peças necessárias ${
+      lossPercent <= 0 ? "" : ", considerando a taxa de perca"
+    }: ${finalItems}`
+  );
 }
 
-function calcularTinta(){
+function calcularTinta() {
   let convenientArea = parseFloat($("#total-area-input").val());
   let openArea = parseFloat($("#tint-total-open-area-input").val());
   let totalCoat = parseFloat($("#tint-coat-input").val());
   let lossPercent = $("#tint-loss-percent-button").is(":checked") ? 10 : 0;
 
-  if($("#unknow-area-button").is(":checked")){
+  if ($("#unknow-area-button").is(":checked")) {
     let width = parseFloat($("#tint-convenient-width-input").val());
     let height = parseFloat($("#convenient-height-input").val());
 
@@ -60,15 +90,35 @@ function calcularTinta(){
   0.04 para tinta acrilica padrao
   */
 
-  const rendimento =  0.180;
+  const rendimento = 0.18;
 
   let finalValue = convenientArea * rendimento;
 
   finalValue = finalValue * totalCoat;
 
+  finalValue =
+    lossPercent == 0
+      ? finalValue.toFixed(2)
+      : (finalValue * (1 + lossPercent / 100)).toFixed(2);
 
-  finalValue = lossPercent == 0 ? finalValue.toFixed(2) : (finalValue * (1 + (lossPercent / 100))).toFixed(2);
+  $("#tint-result").html(
+    `Quantidade de litros necessárias${
+      lossPercent == 0 ? "" : ", considerando a taxa de perca"
+    } : ${finalValue}`
+  );
+}
 
-  $("#tint-result").html(`Quantidade de litros necessárias${lossPercent == 0 ? "" : ", considerando a taxa de perca"} : ${finalValue}`)
+
+function calcularTelha(){
+  let width = parseFloat($('#tile-convenient-width-input').val());
+  let length = parseFloat($('#tile-convenient-length-input').val());
+  let lossPercent = $('#tile-loss-percent-button').is(':checked') ? 10 : 0;
+  let area = width * length;
+  let unitsPerSquareMeter = parseFloat($('#tiles-type').val());
+
+  let totalUnits = (area * unitsPerSquareMeter).toFixed(0);
+  let finalUnits = lossPercent === 0 ? totalUnits : (totalUnits * (1 + lossPercent / 100)).toFixed(0);
+
+  $('#tile-result').html(`Quantidade de peças necessárias ${lossPercent <= 0 ? "" : ", considerando a taxa de perda"}: ${finalUnits}`);
 
 }
