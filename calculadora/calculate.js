32d493604720a8a1d71f013ea0010e33aea96165
Marco Antonio <67771574+tonho991@users.noreply.github.com>
2024-07-19 17:00:55 -0400

    19/07

--- calculadora/calculate.js
+++ calculadora/calculate.js
@@ -1,7 +1,59 @@
-$("#modal-telha").modal("show");
+let searchParams = new URLSearchParams(window.location.search);
+let type = searchParams.get("type");
+let length = searchParams.get("length");
+
+if (length) {
+  if (length.indexOf("x") < 0) {
+    length = ["120", "160"]
+  } else {
+    length = length.toLowerCase().split("x");
+
+    if (length.length != 2 && (!isNaN(length[0]) || !isNaN(length[0]))) {
+      length = ["120", "160"]
+    }
+  }
+}
+
+if (type) {
+  switch (type) {
+    case "piso":
+      if (length) $("#floorDialogTitle").html(`Calcular Piso (${length[0]}x${length[1]})`)
+      $("#modal-piso").modal("show");
+      break;
+    case "tinta":
+      $("#tintDialogTitle").html("Calcular Uso de Tinta");
+      $("#modal-tinta").modal("show");
+      break;
+    case "massa-corrida":
+      $("#tintDialogTitle").html("Calcular Uso de Massa Corrida");
+      $("#modal-tinta").modal("show");
+      break;
+    case "selador":
+      $("#tintDialogTitle").html("Calcular Uso de Selador");
+      $("#modal-tinta").modal("show");
+      break;
+    case "telha":
+      $("#modal-telha").modal("show");
+      break;
+    default:
+      break;
+  }
+}
+
+$("#show-modal-floor").on("click", () => {
+  $("#modal-piso").modal("show");
+});
+
+$("#show-modal-tint").on("click", () => {
+  $("#modal-tinta").modal("show");
+});
+
+$("#show-modal-tile").on("click", () => {
+  $("#modal-telha").modal("show");
+});
 
 $("#btn-calc").on("click", () => {
-  calculate();
+  calcularPiso();
 });
 
 $("#tint-btn-calc").on("click", () => {
@@ -44,14 +96,22 @@ $("#tiles-type").change(function () {
   $("#calculate-content").show();
 });
 
-function calculate() {
-  let convenientWidth = parseFloat($("#convenient-width-input").val());
-  let convenientLength = parseFloat($("#convenient-length-input").val());
+function calcularPiso() {
+  let convenientWidth = parseFloat($("#convenient-width-input").val()) || 0;
+  let convenientLength = parseFloat($("#convenient-length-input").val()) || 0;
   let lossPercent = $("#loss-percent-button").is(":checked") ? 10 : 0;
 
-  //exemplo de piso 120x60
 
-  let itemArea = (120 / 100) * (100 / 60);
+  if (convenientWidth <= 0 || convenientLength <= 0) {
+    $("#result").html("Os valores nao pode ser menor que 0.");
+    return;
+  }
+
+  const itemHeight = parseFloat(length[0]);
+  const itemWidth = parseFloat(length[1]);
+
+  //exemplo de piso 120x60
+  let itemArea = (itemWidth / 100) * (100 / item);
   let convenientArea = convenientWidth * convenientLength;
 
   let items = (convenientArea / itemArea).toFixed(0);
@@ -67,10 +127,24 @@ function calculate() {
 }
 
 function calcularTinta() {
-  let convenientArea = parseFloat($("#total-area-input").val());
-  let openArea = parseFloat($("#tint-total-open-area-input").val());
-  let totalCoat = parseFloat($("#tint-coat-input").val());
+  let convenientArea = parseFloat($("#total-area-input").val()) || 0;
+  let openArea = parseFloat($("#tint-total-open-area-input").val()) || 0;
+  let totalCoat = parseFloat($("#tint-coat-input").val()) || 0;
   let lossPercent = $("#tint-loss-percent-button").is(":checked") ? 10 : 0;
+
+  if (type) {
+    switch (type) {
+      case "piso":
+
+      case "massa-corrida":
+        $("tintDialogTitle").html("Calcular Uso de Massa Corrida");
+        break;
+      case "selador":
+        $("tintDialogTitle").html("Calcular Uso de Selador");
+        break;
+    }
+
+  }
 
   if ($("#unknow-area-button").is(":checked")) {
     let width = parseFloat($("#tint-convenient-width-input").val());
@@ -78,6 +152,12 @@ function calcularTinta() {
 
     convenientArea = width * height;
   }
+
+  if (convenientArea <= 0 || openArea <= 0 || totalCoat <= 0) {
+    $("#tint-result").html("Os valores nao podem ser menor que 0 ou nulo.");
+    return;
+  }
+
 
   convenientArea = convenientArea - openArea;
 
@@ -89,7 +169,13 @@ function calcularTinta() {
   0.04 para tinta acrilica padrao
   */
 
-  const rendimento = 0.18;
+  let rendimento;
+
+  if (type == "selador") rendimento = 0.180;
+  else if (type == "massa-corrida") rendimento = 0.572;
+  else rendimento = 0.04;
+
+  console.log(rendimento);
 
   let finalValue = convenientArea * rendimento;
 
@@ -101,21 +187,26 @@ function calcularTinta() {
       : (finalValue * (1 + lossPercent / 100)).toFixed(2);
 
   $("#tint-result").html(
-    `Quantidade de litros necessárias${lossPercent == 0 ? "" : ", considerando a taxa de perca"
-    } : ${finalValue}`
+    `Quantidade <strong>aproximado</strong> de litros necessários ${lossPercent == 0 ? "" : ", considerando a taxa de perca"
+    } : ${finalValue}L`
   );
 }
 
 
 function calcularTelha() {
-  let width = parseFloat($('#tile-convenient-width-input').val());
-  let length = parseFloat($('#tile-convenient-length-input').val());
+  let width = parseFloat($('#tile-convenient-width-input').val()) || 0;
+  let length = parseFloat($('#tile-convenient-length-input').val()) || 0;
   let lossPercent = $('#tile-loss-percent-button').is(':checked');
-  let unitsPerSquareMeter = parseFloat($('#tiles-type').val());
-  let slope = parseFloat($('#roof-degree-input').val());
+  let unitsPerSquareMeter = parseFloat($('#tiles-type').val()) || 0;
+  let slope = parseFloat($('#roof-degree-input').val()) || 0;
+
+  if (width <= 0 || length <= 0 || unitsPerSquareMeter <= 0) {
+    $("#tile-result").html("Os valores nao podem ser menor que 0 ou nulo.");
+    return;
+  }
 
   let area = width * length;
-  
+
   let tiles = 0;
 
   if (slope !== 0) {
